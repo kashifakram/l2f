5241ee36a14f81438d5bf94975e430fa1fb1a56a
Kashif Akram <k.akram@outlook.cm>
2021-05-06 20:07:16 +1000

    chores: nav drawer bottom

--- components/base/Dialogue.vue
+++ components/base/Dialogue.vue
@@ -1,10 +1,9 @@
 <template>
-  <div class="text-center">
+  <v-row justify="center">
     <v-dialog
       v-model="dialog"
-      overlay-color="transparent"
-      class="align-center"
       transition="dialog-bottom-transition"
+      hide-overlay
     >
       <template v-slot:activator="{ on, attrs }">
         <v-btn
@@ -17,29 +16,91 @@
         </v-btn>
       </template>
 
-<div class="clippedDialog blue darken-4">
-        <div class="grey darken-4 text-center text-h6">
-          Privacy Policy
-        </div>
-    <div class="dtext text-center">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-    </div>
-    <div class="text-center"> 
-        <div>
+<v-card class="clippedDialog text-center mx-auto" width="900">
+        <v-toolbar
+          dark
+          color="transparent"
+          class="text-center mx-auto"
+          width="700"
+          
+        >
           <v-btn
-            color="secondary"
-            text
+            icon
+            dark
             @click="dialog = false"
           >
-            I accept
+            <v-icon>mdi-close</v-icon>
           </v-btn>
-        </div>
-        </div>
-      </div>
+          <v-toolbar-title>Settings</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-toolbar-items>
+            <v-btn
+              dark
+              text
+              @click="dialog = false"
+            >
+              Save
+            </v-btn>
+          </v-toolbar-items>
+        </v-toolbar>
+        <v-list
+          three-line
+          subheader
+                  class="text-center mx-auto"
+          width="800"
+        >
+          <v-subheader>User Controls</v-subheader>
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>Content filtering</v-list-item-title>
+              <v-list-item-subtitle>Set the content filtering level to restrict apps that can be downloaded</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>Password</v-list-item-title>
+              <v-list-item-subtitle>Require password for purchase or use password to restrict purchase</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+        <v-divider></v-divider>
+        <v-list
+          three-line
+          subheader
+                  class="text-center mx-auto"
+          width="800">
+          <v-subheader>General</v-subheader>
+          <v-list-item>
+            <v-list-item-action>
+              <v-checkbox v-model="notifications"></v-checkbox>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>Notifications</v-list-item-title>
+              <v-list-item-subtitle>Notify me about updates to apps or games that I downloaded</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-action>
+              <v-checkbox v-model="sound"></v-checkbox>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>Sound</v-list-item-title>
+              <v-list-item-subtitle>Auto-update apps at any time. Data charges may apply</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-action>
+              <v-checkbox v-model="widgets"></v-checkbox>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>Auto-add widgets</v-list-item-title>
+              <v-list-item-subtitle>Automatically add home screen widgets</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-card>
     </v-dialog>
-  </div>
+  </v-row>
 </template>
 
 <script>
@@ -56,7 +117,7 @@
   .clippedDialog {
     /* clip-path: polygon(0% 0%, 10% 100%, 90% 100%, 100% 0%); */
     clip-path: polygon(0 15%, 15% 0, 85% 0%, 100% 15%, 100% 85%, 85% 100%, 15% 100%, 0% 85%);
-    border: 5px black solid;
+  
   }
 
     .clippedDFooter {
